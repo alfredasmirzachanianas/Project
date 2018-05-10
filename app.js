48a6c06f89f346b3cf787b7faf071345fed806e8
alfredasmirzachanianas <alfredasmirzachanianas@yahoo.com>
2018-05-10 21:12:18 +0300

    Fix

--- app.js
+++ app.js
@@ -9,11 +9,30 @@ mongoose.connect('mongodb+srv://alfre:asasas@cluster0-rkayj.mongodb.net/vatrates
 
 app.use(bodyParser.json());
 
+
 var db = mongoose.connection;
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(3001, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
+});
+
+app.use(function (req, res, next) {
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
 });
 
 app.get('/', function(req,res){
--- src/App.js
+++ src/App.js
@@ -1,19 +1,108 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        vatrates: []
+    };
+  }
+  componentDidMount() {
+      fetch('http://localhost:3001/api/vatrates/')
+        .then(res => res.json())
+        .then(res => {
+              this.setState({
+                  vatrates: res
+               });
+        })
+        .catch(error => {
+            console.log(error);
+        })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return(
+        /*
+      <div>
+          <h3>VAT rates</h3>
+          <table>
+              <thead>
+                   <tr>
+                      <th>id</th>
+                      <th>country</th>
+                      <th>standard_rate</th>
+                      <th>reduced_rate</th>
+                      <th>reduced_rate_alt</th>
+                      <th>super_reduced_rate</th>
+                      <th>parking_rate</th>
+                      <th>createDate</th>
+                      <th>modifyDate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.vatrates.map(function(p,index){
+                      return (
+                          <tr>
+                              <td>{p._id}</td>
+                              <td>{p.country}</td>
+                              <td>{p.reduced_rate}</td>
+                              <td>{p.reduced_rate_alt}</td>
+                              <td>{p.super_reduced_rate}</td>
+                              <td>{p.parking_rate}</td>
+                              <td>{p.createDate}</td>
+                              <td>{p.modifyDate}</td>
+                          </tr>
+                      );
+
+                    })}
+                  </tbody>
+            </table>
+        </div>
+        */
+       <div class="container-fluid">
+       <h1>VATRates</h1>
+
+       <p>This page tests the output of JSON API</p>
+
+       <div class="row">
+
+               <table class="table table-striped table-hover table-sm">
+                   <thead class="thead-inverse">
+                       <tr>
+                           <th class="country-code">Country Code</th>
+                           <th class="country">Country Name</th>
+                           <th class="rate rate-reduced">Standard rate </th>
+                           <th class="rate rate-standard">Reduced rate</th>
+                           <th class="rate rate-parking">Reduced rate alt</th>
+                           <th class="rate rate-parking">Super reduced rate</th>
+                           <th class="rate rate-parking">Parking rate</th>
+                           <th class="rate rate-date">CreateDate</th>
+                           <th class="rate rate-date">ModifyDate</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                    {this.state.vatrates.map(function(p,index){
+                      return (
+                          <tr>
+                              <td>{p._id}</td>
+                              <td>{p.country}</td>
+                              <td>{p.standard_rate}</td>
+                              <td>{p.reduced_rate}</td>
+                              <td>{p.reduced_rate_alt}</td>
+                              <td>{p.super_reduced_rate ? "true" : "false"}</td>
+                              <td>{p.parking_rate}</td>
+                              <td>{p.createDate}</td>
+                              <td>{p.modifyDate}</td>
+                          </tr>
+                      );
+
+                    })}
+                  </tbody>
+               </table>
+           </div>
+        </div>
     );
   }
 }
